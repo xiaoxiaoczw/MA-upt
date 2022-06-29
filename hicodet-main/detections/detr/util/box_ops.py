9ffb42b1e4028df87a3e01f63afd81fd0e977ca4
xiaoxiaoczw <9518143@qq.com>
2022-06-29 19:04:52 +0200

    change bbox if condition

--- hicodet-main/detections/detr/util/box_ops.py
+++ hicodet-main/detections/detr/util/box_ops.py
@@ -48,8 +48,8 @@ def generalized_box_iou(boxes1, boxes2):
     """
     # degenerate boxes gives inf / nan results
     # so do an early check
-    assert (boxes1[:, 2:] >= boxes1[:, :2]).all()  # default
-    assert (boxes2[:, 2:] >= boxes2[:, :2]).all()  # default
+    # assert (boxes1[:, 2:] >= boxes1[:, :2]).all()  # default
+    # assert (boxes2[:, 2:] >= boxes2[:, :2]).all()  # default
     assert (boxes1[:, 2:] > boxes1[:, :2]).all()
     assert (boxes2[:, 2:] > boxes2[:, :2]).all()
 
