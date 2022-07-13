680669dacb0caeb8c1640296c75938ed06264df0
xiaoxiaoczw <9518143@qq.com>
2022-07-13 15:11:09 +0200

    add vidhoi dataset num_classes

--- upt/detr/models/detr.py
+++ upt/detr/models/detr.py
@@ -315,6 +315,10 @@ def build(args):
         # for panoptic, we just add a num_classes that is large enough to hold
         # max_obj_id + 1, but the exact value doesn't really matter
     #    num_classes = 250
+    if args.dataset == 'hicodet':
+        num_classes = 80
+    if args.dataset == 'vidhoi':
+        num_classes = 78
     device = torch.device(args.device)
 
     backbone = build_backbone(args)
